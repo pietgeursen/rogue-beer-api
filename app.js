16dc4fe2f086fdc3b2ba0adae7fe3c92efa8ffae
Piet Geursen <pietgeursen@gmail.com>
2016-11-20 19:02:39 +1300

    Add cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var beers = require('./routes/beers')
 var styles = require('./routes/styles')
 
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
